19b0c82d5118e5366a5171b8561249ebc4a24cd5
rakdav <rakdav@inbox.ru>
2025-10-29 14:16:23 +0200

    1

--- app/src/main/java/com/example/retrofitexample/viewmodel/PokemonListViewModel.kt
+++ app/src/main/java/com/example/retrofitexample/viewmodel/PokemonListViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.retrofitexample.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.Recomposer
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.retrofitexample.data.model.PokemonListModel
 import com.example.retrofitexample.data.repository.PokemonListRepository
 import com.example.retrofitexample.data.repository.PokemonListRepositoryInterface
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class PokemonListViewModel(
     private val repository: PokemonListRepositoryInterface= PokemonListRepository()
@@ -20,7 +23,27 @@ class PokemonListViewModel(
     val errorMessage: StateFlow<String?> get() = _errorMessage.asStateFlow()
     val isLoading: StateFlow<Boolean> get() = _isLoading.asStateFlow()
     fun getPokemonList(){
-
+        viewModelScope.launch {
+            _isLoading.value=true
+            val response=repository.getPokemonList(0,150)
+            if(response.isSuccessful){
+                val body=response.body()
+                if(body!=null){
+                    Log.d("Success","$body?.size")
+                    _isLoading.value=false
+                    _pokemonList.value=body
+                }
+                else
+                {
+                    val error=response.errorBody()
+                    if(error!=null){
+                        Log.d("Pokemon list error",error.string())
+                        _isLoading.value=false
+                        _errorMessage.value=error.toString()
+                    }
+                }
+            }
+        }
     }
 
 }
